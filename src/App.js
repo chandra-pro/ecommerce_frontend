3758029cc6457268e9ae368ca606ca8133d54d66
chandra-pro <goluchandra1812@gmail.com>
2023-08-11 13:02:21 +0530

    fetchproducts

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   });
 
   const fetchProducts = async () => {
-    const res = await fetch(`https://dummyjson.com/products?limit=100`);
+    const res = await fetch(`https://dummyjson.com/products`);
     const data = await res.json();
 
     console.log(data);
